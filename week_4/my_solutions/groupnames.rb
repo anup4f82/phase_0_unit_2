c9ec4fa68272eea128a9043d2c80a8be1b4b31e4
Anup <anup-pradhan@hotmail.com>
2014-04-03 20:25:25 -0700

    Added Solo challenge and Accountability group creator.

--- week_4/my_solutions/groupnames.rb
+++ week_4/my_solutions/groupnames.rb
@@ -1,63 +1,201 @@
-def acct(names)
-    
-    len = names.length
-    g = len/4
-    rem = len %4
 
-  
-  unit = Hash.new()
-  
-  for n in 1..3
-  
-  groups =  Hash.new()
-  
-  
-  group = []
-  used = []
-  
- 
-  
-for i in 1..g
-  
-  for s in 1..4
-  
-  
- 
-  a = 0+ rand(names.length-0)
-  
- if (used.include?a)
-      a = 0+ rand(names.length-0)
+#Pseudocode
+
+# Create a array called fence_lizards and put 27 names in it
+
+# Create a method called acct_groups that takes the array fence_lizards
+
+# Declare a empty hash called unit
+# for n in 1 to 3
+# declare an empty array called used
+# Declare a hash called groups
+# Declare two variables i = 0 and m = 1
+# Create 6 Keys in the groups hash and assign it a empty array
+# for i in 1.. length of the array
+# Used the ruby random number generator logic and assign it to a
+# a = min + 0+ rand(max-min)
+# if a is in array used, redo and get another randon number
+# once a unique number is found put it in used array
+# add the value in fence_lizards[a] to the group m hash(m will have values from 1 to 6 for 6 groups)
+# increment m by 1, if m = 7, set m back to 1
+# Keep looping till all the elements of the array fence_lizards are assigned to a group 
+# Each time it loops it will assign to group[m] where initial value of m = 1 and increment it in the loop till it reaches 6
+# assign Unit[unit1] to group
+# loop through and assign Unit[unit2] and unit[unit3] as well...
+
+def acct_groups(fence_lizards)
+    unit = Hash.new()
+    for n in 1..3
+used = []
+groups = Hash.new(0)
+i = 0
+m = 1
+groups["group1"]=  [] 
+groups["group2"] = []
+groups["group3"] = []
+groups["group4"] = []
+groups["group5"] = []
+groups["group6"] = []
+
+for i in 1..fence_lizards.length
+
+   a = 0+ rand(fence_lizards.length-0)
+
+    if (used.include?a)
+    a = 0+ rand(fence_lizards.length-0)
      redo
-     end    
-  used << a
-  
-  
-  
- group << names[a]
-    
-    s+=1
+     end  
+      used << a
+       groups["group"+m.to_s] << fence_lizards[a]
+        m = m+1
+        if m==7
+         m = 1
      end
-    
-    groups["group"+i.to_s] = group
-    
-    group =[]
-    
-    i+=1
-   
+     
 end
+ unit["unit"+n.to_s] = groups
+ end
+
+
+
+
+
+#initial Solution
+
+# def acct(names)
+#len = names.length
+# g = len/4
+#rem = len %4
+# unit = Hash.new()
+  #   for n in 1..3
+  #   groups =  Hash.new()
+  #   group = []
+#   used = []
+  # for i in 1..g
+  #   for s in 1..4
+  #   a = 0+ rand(names.length-0)
+  #  if (used.include?a)
+#       a = 0+ rand(names.length-0)
+#      redo
+#      end    
+#   used << a
+#  group << names[a]
+    #     s+=1
+#      end
+    #     groups["group"+i.to_s] = group
+    #     group =[]
+    #     i+=1
+   # end
+ # unit["unit"+n.to_s] = groups
+# n+=1
+# end
+# for i in 1..3
+# puts
+# puts "unit#{i} groups are:"
+# puts
+# puts"Group1: #{unit["unit#{i}"]["group1"].join(",")}"
+# puts "Group2: #{unit["unit#{i}"]["group2"].join(",")}"
+# puts "Group3: #{unit["unit#{i}"]["group3"].join(",")}"
+# puts "Group4: #{unit["unit#{i}"]["group4"].join(",")}"
+# puts "Group5: #{unit["unit#{i}"]["group5"].join(",")}"
+# puts "Group6: #{unit["unit#{i}"]["group6"].join(",")}"
+# end
+# end
+
+
+#refactored solution
+
+fence_lizards = ["Adam Dziuk","Adam Ryssdal","Aki Suzuki","Allison Wong","Andra Lally","Andrew McDonald","Anup Pradhan",
+  "CJ Jameson","Christopher Aubuchon","Clark Hinchcliff","Devin A Johnson","Dominick Oddo","Dong Kevin Kang","Eiko Seino",
+  "Eoin McMillan","Eric Wehrli","Hunter T. Chapman","Jacob Persing","Jon Pabico","Mary Huerster","Parjam Davoody","Samuel Davis",
+  "Sebastian Belmar","Shawn Seibert ","William Butler Bushyhead","Yuzu Saijo","christiane kammerl"]
+
+
+
+def acct_groups(fence_lizards)
+    unit = Hash.new()
+    for n in 1..3
+used = []
+groups = Hash.new(0)
+i = 0
+m = 1
+groups["group1"]=  [] 
+groups["group2"] = []
+groups["group3"] = []
+groups["group4"] = []
+groups["group5"] = []
+groups["group6"] = []
+
+for i in 1..fence_lizards.length
+
+   a = 0+ rand(fence_lizards.length-0)
+
+    if (used.include?a)
+    a = 0+ rand(fence_lizards.length-0)
+     redo
+     end  
+      used << a
+       groups["group"+m.to_s] << fence_lizards[a]
+        m = m+1
+        if m==7
+         m = 1
+     end
+     
+end
+ unit["unit"+n.to_s] = groups
+ end
+  
+#   for i in 1..3
+# puts
+# puts "UNIT #{i} groups are:"
+# puts
+# puts "Group1:\n#{unit["unit#{i}"]["group1"].join("\n")}"
+# puts
+# puts "Group2:\n#{unit["unit#{i}"]["group2"].join("\n")}"
+# puts
+# puts "Group3:\n#{unit["unit#{i}"]["group3"].join("\n")}"
+# puts
+# puts "Group4:\n#{unit["unit#{i}"]["group4"].join("\n")}"
+# puts
+# puts "Group5:\n#{unit["unit#{i}"]["group5"].join("\n")}"
+# puts
+# puts "Group6:\n#{unit["unit#{i}"]["group6"].join("\n")}"
+
+# end
  
-unit["unit"+n.to_s] = groups
-n+=1
-end
-for i in 1..3
-puts
-puts "unit#{i} groups are:"
-puts
-puts"Group1: #{unit["unit#{i}"]["group1"].join(",")}"
-puts "Group2: #{unit["unit#{i}"]["group2"].join(",")}"
-puts "Group3: #{unit["unit#{i}"]["group3"].join(",")}"
-puts "Group4: #{unit["unit#{i}"]["group4"].join(",")}"
-puts "Group5: #{unit["unit#{i}"]["group5"].join(",")}"
-puts "Group6: #{unit["unit#{i}"]["group6"].join(",")}"
-end
-end
+ return unit
+ end
+ 
+ 
+ # Driver test code
+ p acct_groups(fence_lizards).class == Hash 
+  p acct_groups(fence_lizards)["unit1"].length==6
+  p acct_groups(fence_lizards)["unit2"].length==6
+  p acct_groups(fence_lizards)["unit3"].length==6
+
+p acct_groups(fence_lizards)["unit1"]["group1"] &  acct_groups(fence_lizards)["unit1"]["group2"] & acct_groups(fence_lizards)["unit1"]["group3"] & acct_groups(fence_lizards)["unit1"]["group4"] & acct_groups(fence_lizards)["unit1"]["group5"]  & acct_groups(fence_lizards)["unit1"]["group6"] == []
+ 
+p acct_groups(fence_lizards)["unit2"]["group1"] &  acct_groups(fence_lizards)["unit2"]["group2"] & acct_groups(fence_lizards)["unit2"]["group3"] & acct_groups(fence_lizards)["unit2"]["group4"] & acct_groups(fence_lizards)["unit2"]["group5"]  & acct_groups(fence_lizards)["unit2"]["group6"] == []
+
+p acct_groups(fence_lizards)["unit3"]["group1"] &  acct_groups(fence_lizards)["unit3"]["group2"] & acct_groups(fence_lizards)["unit3"]["group3"] & acct_groups(fence_lizards)["unit3"]["group4"] & acct_groups(fence_lizards)["unit3"]["group5"]  & acct_groups(fence_lizards)["unit3"]["group6"] == []
+
+
+#Reflection
+
+# What parts of your strategy worked? What problems did you face?
+# # My initial solution was only working if the number of people in the array is divisible by 4.. I guess I wanted to get started to just came up with the whatever
+# I could think of.
+# What questions did you have while coding? What resources did you find to help you answer them?
+# I had to research on how to generate random number, redo loop, How to have the value of hash as array.
+# I also was confused with the 'Driver test code'. But finally got it. I wanted to output the names in each unit and each of the 6 groups. I havve the code
+# for it, But then it wasnt testing all my 'driver code'.. I need to figure it out. It was only outputting the first driver test code result
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+# None. Like I said I had to look up random number generatot and redo loop syntax
+# Did you learn any new skills or tricks?
+#redo loop,random number, 
+# How confident are you with each of the learning objectives?
+# very confident
+# Which parts of the challenge did you enjoy?
+# it was fun to see the Output
+# Which parts of the challenge did you find tedious?
+# figuring out the driver test code 
--- week_4/my_solutions/my_solution.rb
+++ week_4/my_solutions/my_solution.rb
@@ -1,27 +1,38 @@
 
-
-
-
+#Pseudocode
+# Create a Method Separate_comma that takes a Number n(Fixnum)
+# if the n < 1000 return Number
+# else
+# Split the number and put in a array using Split method
+# Declare two variables s and t and set them to 0
+#Declare variable i and set to length of the array
+# While i > 3 (In my code I put a commma before the last three elements of the array and then extract the array before it. and then set i to the new length
+  #Only if i > 3 I continue the loop)
+# increment s by 1
+# Set variable a = 3*s+1 [ I use this variable to decide the position I will put the comma in. using the formula length of array -a ]
+# Insert a comma in the position [(length of array) - a] and name this array 'new'
+# Create a new array called new1 which will have the elements of array before the ','
+# Set variable i to length of array new1
+#increment t by 1
+#Continue looping till all the commas are placed and number of elements before the comma are less than 3
+# return the Number by using Join.
 
 # initial solution
 # def separate_comma(n)
-    
 #     if n <1000
 #         return n.to_s
        
-        
 #     elsif (n> 999 && n <1000000)
 #      new = n.to_s.split("")
 #      new.insert(new.length-3,',')
 #      return new.join("")
      
-      
-    
 #     elsif (n> 999999 && n <10000000000)
 #      new = n.to_s.split("")
 #     new1 = new.insert(new.length-3,',')
 #      new2 = new1.insert(new1.length-7,',')
 #      return new2.join("")
+# 
   
 # end
 #    end
@@ -34,17 +45,46 @@ if n <1000
 else
   new = n.to_s.split("")
   s = 0
-  i = n.length
+  t = 0
+  i = new.length
+  
     while i > 3
       s = s+1
-      a = 3*s
+      a = 3*s+t
       new.insert(new.length-a,',')
-      new1 = new[0..new.length-(a+1)]
+      new1 = new[0..new.length-(a+2)]
       i = new1.length
-        
+        t = t+1
     end
     
     return new.join("")
 end
 
 end
+
+# Driver testCode
+
+puts separate_comma(1000) == "1,000"
+puts separate_comma(100) == "100"
+puts separate_comma(1000000) == "1,000,000"
+puts separate_comma(10000000) == "10,000,000"
+
+# Reflection
+
+# What parts of your strategy worked? What problems did you face?
+
+# Initially when I started coding and came up with the solution it was working only for numbers upto 10000000000. But then I had to think about making the solution
+#effecient enough so it will work with any number. Just coming up with the logic took a little time. Had to test the code and make changes to get the desired
+#result
+# What questions did you have while coding? What resources did you find to help you answer them?
+# I havent used the 'insert' method much, So had to look up the syntax.
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+# None
+# Did you learn any new skills or tricks?
+# Yes Array insert
+# How confident are you with each of the learning objectives?
+# Very
+# Which parts of the challenge did you enjoy?
+# Yes this was fun
+# Which parts of the challenge did you find tedious?
+# It wasnt tedious at all
