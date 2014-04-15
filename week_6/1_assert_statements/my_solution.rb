5dc4af4d2ada06d3b885cbd655c869a6225d6c12
Anup <anup-pradhan@hotmail.com>
2014-04-14 22:26:52 -0700

    added assert statements for week_6 - 1_assert_statements

---
+++ week_6/1_assert_statements/my_solution.rb
@@ -0,0 +1,91 @@
+# U2.W6: Testing Assert Statements
+
+# I worked on this challenge [by myself, with: ].
+
+
+# 2. Review the simple assert statement
+
+def assert
+  raise "Assertion failed!" unless yield
+end
+
+name = "bettysue"
+assert { name == "bettysue" }
+assert { name == "billybob" }
+
+# 2. Pseudocode what happens when the code above runs
+
+#In the above code we have a method assert. It raises an exception when yield returns false.
+#yield in the above case invokes the block and if name == bettysue it returns true if name is not bettysue it returns false
+
+
+
+
+# 3. Copy your selected challenge here
+
+class CreditCard
+
+
+def initialize(card)
+
+@card = card
+if @card.to_s.length !=16
+raise ArgumentError, 'Digits not equal to 16'
+end
+end
+
+def check_card
+
+ card_num = @card.to_s.split("")
+ card_num.map!{|x| x.to_i}
+ i = -1
+  card_num = card_num.map{|x| i+=1;(i %2 == 0 ? x*2 :x) }
+ card_num = card_num.map{|x| (x.to_s.length > 1? x.to_s.split("") :x)}.flatten.map{|x| x.to_i}
+
+
+if  (card_num.reduce(:+) % 10 == 0)
+    return true
+else
+    return false
+end
+end
+
+
+end
+
+
+
+
+# 1. DRIVER TESTS GO BELOW THIS LINE
+
+
+
+valid = CreditCard.new(4563960122001999)
+
+ p (valid.check_card) == true
+invalid = CreditCard.new(1111111111111111)
+ p (invalid.check_card) == false
+
+ p (valid.class) == CreditCard
+
+
+
+# 4. Convert your driver test code from that challenge into Assert Statements
+
+
+def assert
+    
+  raise "Assertion failed" unless yield 
+end
+
+ valid = CreditCard.new (4563960122001999)
+
+
+invalid = CreditCard.new(1111111111111111)
+assert {invalid.check_card == false}
+ assert {valid.check_card == true}
+ assert {valid.class == CreditCard}
+
+
+
+# 5. Reflection
