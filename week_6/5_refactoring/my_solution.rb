e2da169fc6ef3f6f53a6ffb3b8faa3b020616260
Anup <anup-pradhan@hotmail.com>
2014-04-19 16:07:28 -0700

    updated reflection for 5_refactoring

--- week_6/5_refactoring/my_solution.rb
+++ week_6/5_refactoring/my_solution.rb
@@ -1,33 +1,33 @@
 # U2.W6: Refactoring for Code Readability
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with Yuzu Saijo
 
 
 # Original Solution
 
-class CreditCard
+# class CreditCard
 
-def initialize(array)
-	@array = array
-   if @array.length != 16
-     return "Argument Error"
-     break
-   end 
-end
+# def initialize(array)
+# 	@array = array
+#    if @array.length != 16
+#      return "Argument Error"
+#      break
+#    end 
+# end
 
-def check_card
-	@double = @array.map {|x| x*2}
-	@bynine = @double.map {|x| x%9}
-	@sum = @bynine.inject{|x,y| x+y}
-	if @sum % 10 == 0
-		true
-	else
-		false
-	end
-end
+# def check_card
+# 	@double = @array.map {|x| x*2}
+# 	@bynine = @double.map {|x| x%9}
+# 	@sum = @bynine.inject{|x,y| x+y}
+# 	if @sum % 10 == 0
+# 		true
+# 	else
+# 		false
+# 	end
+# end
 
-end
+# end
 
 
 
@@ -45,18 +45,14 @@ def initialize(card)
    end 
 end
 
-# Starting with the second to last digit, double every other digit until you reach the first digit
 
-# Sum all the untouched digits and the doubled digits (doubled digits need to be broken apart, 10 becomes 1 + 0)
-
-# If the total is a multiple of ten, you have received a valid credit card number!
 
 def check_card
 	card_number = @card.to_s.split("")
 	double_arr = []
 	i = 0
 	card_number.map!{|x| x.to_i}
-    card_number.each do |x|
+  card_number.each do |x|
       if i%2 == 0
         double_arr << x*2
       else
@@ -66,7 +62,7 @@ def check_card
     end
     
     sum = 0
-     double_arr.map!{|x| x.to_i}
+    double_arr.map!{|x| x.to_i}
     double_arr.each do |y|
       if y<10
         sum+=y
@@ -84,12 +80,6 @@ end
 
 end
 
-
-
-
-
-
-
 # DRIVER TESTS GO BELOW THIS LINE
 
 def assert
@@ -104,3 +94,19 @@ assert{creditcard_bad.check_card == false}
 
 
 # Reflection 
+
+# What parts of your strategy worked? What problems did you face?
+# Having done this challenge in week 5 helped. When we re-factored the solution, we initially got an error, but figured out why its failing pretty quickly
+
+# What questions did you have while coding? What resources did you find to help you answer them?
+# None
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+# None. we just had some errors when we tested the code , but we fixed it
+# Did you learn any new skills or tricks?
+# Yes Yuzu had a new way to code the valid credit card and I had not thought of that approach
+# How confident are you with each of the Learning Competencies?
+# Very confident
+# Which parts of the challenge did you enjoy?
+# Learning a new way to code the solution
+# Which parts of the challenge did you find tedious?
+# None
