c14c797022315b9fc1e01cd967aea8a12ebb4757
Anup <anup-pradhan@hotmail.com>
2014-04-19 13:55:05 -0700

    Adding solutions for 2,3 and 5

--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -1,62 +1,70 @@
 # U2.W6: Drawer Debugger
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with Butler Bushyhead
 
 
 # 2. Original Code
 
 class Drawer
 
-attr_reader :contents
+  attr_reader :contents
+  
+ 
 
 # Are there any more methods needed in this class?
 
-def initialize
-@contents = []
-@open = true
+  def initialize
+    @contents = []
+    @open = true
+  end
+
+  def open
+    @open = true
+  end
+
+  def close
+    @open = false
+  end 
+
+  def add_item(item)
+    @contents << item
+  end
+
+  def remove_item(item = @contents.pop) #what is `#pop` doing?
+    @contents.delete(item)
+  end
+
+  def dump  # what should this method return?
+    puts "Your drawer is empty."
+    @contents = []
+  end
+
+  def view_contents
+    puts "The drawer contains:"
+    @contents.each {|silverware| puts "- " + silverware.type }
+  end
 end
-
-def open
-@open = true
-end
-
-def close
-@open = false
-end 
-
-def add_item
-@contents << item
-end
-
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
-
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
-
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
-
 
 class Silverware
-attr_reader :type
+
+  attr_reader :type
 
 # Are there any more methods needed in this class?
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
-end
+  def initialize(type, clean = true)
+    @type = type
+    @clean = clean
+  end
 
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
+  def eat
+    puts "eating with the #{type}"
+    @clean = false
+  end
+  
+  def clean_silverware
+    @clean = true
+  end
 
 end
 
@@ -86,13 +94,53 @@ silverware_drawer.dump
 silverware_drawer.view_contents #What should this return?
 
 
-fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
-fork.eat
+new_fork = Silverware.new("new_fork") #add some puts statements to help you trace through the code...
+silverware_drawer.add_item(new_fork)
+silverware_drawer.view_contents
+removed_fork = silverware_drawer.remove_item(new_fork)
+removed_fork.eat
+silverware_drawer.view_contents
 
-#BONUS SECTION
-# puts fork.clean
+spork = Silverware.new("spork")
+silverware_drawer.add_item(spork)
+silverware_drawer.add_item(Silverware.new("butter knife")) 
+silverware_drawer.view_contents
+removed_spork = silverware_drawer.remove_item(spork)
+removed_spork.eat
+silverware_drawer.view_contents
 
 # DRIVER TESTS GO BELOW THIS LINE
+# def assert
+#   raise "Assertion failed!" unless yield
+# end
+# test_drawer = Drawer.new
+# assert { test_drawer.view_contents == []}
+
+# test_drawer.add_item(Silverware.new("spoon"))
+# test_drawer.contents
+# test_drawer.view_contents
+# assert { @contents == "spoon" }
+
+# test_drawer.add_item(Silverware.new("fork"))
+# assert { @contents == ["spoon", "fork"] }
+
+def assert
+  raise "Assertion failed!" unless yield
+end
+
+test_drawer = Drawer.new
+assert { test_drawer.view_contents == []}
+
+spoon = Silverware.new("spoon")
+test_drawer.add_item(spoon)
+test_drawer.remove_item
+spoon.eat
+assert {spoon.eat == false}
+test_drawer.dump
+test_drawer.dump == "Your drawer is empty"
+
+
+# 5. Reflection 
 
 
 
--- week_6/5_refactoring/my_solution.rb
+++ week_6/5_refactoring/my_solution.rb
@@ -6,10 +6,84 @@
 
 # Original Solution
 
+class CreditCard
+
+def initialize(array)
+	@array = array
+   if @array.length != 16
+     return "Argument Error"
+     break
+   end 
+end
+
+def check_card
+	@double = @array.map {|x| x*2}
+	@bynine = @double.map {|x| x%9}
+	@sum = @bynine.inject{|x,y| x+y}
+	if @sum % 10 == 0
+		true
+	else
+		false
+	end
+end
+
+end
+
+
 
 
 
 # Refactored Solution
+
+
+class CreditCard
+
+def initialize(card)
+	@card = card
+   if @card.to_s.length != 16
+     return "Argument Error"
+   end 
+end
+
+# Starting with the second to last digit, double every other digit until you reach the first digit
+
+# Sum all the untouched digits and the doubled digits (doubled digits need to be broken apart, 10 becomes 1 + 0)
+
+# If the total is a multiple of ten, you have received a valid credit card number!
+
+def check_card
+	card_number = @card.to_s.split("")
+	double_arr = []
+	i = 0
+	card_number.map!{|x| x.to_i}
+    card_number.each do |x|
+      if i%2 == 0
+        double_arr << x*2
+      else
+        double_arr << x
+      end
+      i+=1
+    end
+    
+    sum = 0
+     double_arr.map!{|x| x.to_i}
+    double_arr.each do |y|
+      if y<10
+        sum+=y
+      else
+        sum+= 1+ y%10
+      end
+    end
+    
+ 	if sum % 10 == 0
+		true
+	else
+		false
+	end
+end
+
+end
+
 
 
 
@@ -18,7 +92,13 @@
 
 # DRIVER TESTS GO BELOW THIS LINE
 
-
+def assert
+ raise "Assertion failed" unless yield
+end
+creditcard = CreditCard.new(4563960122001999)
+creditcard_bad = CreditCard.new(1111111111111111)
+assert{creditcard.check_card == true} 
+assert{creditcard_bad.check_card == false}
 
 
 
